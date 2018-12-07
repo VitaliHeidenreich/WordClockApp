d148fc0abc94018427074c54903c89cdbda979fe
VitaliHeidenreich <vitali.heidenreich.mail@gmail.com>
2018-12-07 17:35:00 +0100

    Optimieren des Layouts

--- app/src/main/java/com/domain/no/wordclock/MainActivity.java
+++ app/src/main/java/com/domain/no/wordclock/MainActivity.java
@@ -18,6 +18,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.support.v7.widget.Toolbar;
@@ -136,6 +137,7 @@ public class MainActivity extends AppCompatActivity /*implements SettingsDialog.
         tbtn.setOnClickListener(btnListener);
 
         txtConnectionstatus.setText("You are not connected!\nPress \"CONNECT\"");
+
     }
 
     @SuppressLint("ClickableViewAccessibility")
